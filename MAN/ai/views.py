295da8548dcfb7eda3974bc3d644b99af8eacab7
AnDr-WaY <andriichampion2008@gmail.com>
2024-12-03 15:32:28 +0200

    updated README and changed coments in views

--- MAN/ai/views.py
+++ MAN/ai/views.py
@@ -13,15 +13,13 @@ def main(request):
     if request.method == 'POST':
         form = UploadImageForm(request.POST, request.FILES)
         if form.is_valid():
-            uploaded_image = form.save()  # Сохраняем изображение
-            image_path = uploaded_image.image.path  # Путь к сохранённому файлу
-
-            # Запускаем проверку через нейросеть YOLO
-            results = model.predict(source=image_path)  # Используем YOLO
+            uploaded_image = form.save()  
+            image_path = uploaded_image.image.path  
+            
+            results = model.predict(source=image_path)  
             probs = results[0].probs  
             predicted_class = probs.top1  
             confidence = int(probs.top1conf.item()*100)
-            print(predicted_class)
             is_mine = predicted_class == 0
             
             os.remove(image_path)
--- README.md
+++ README.md
@@ -9,8 +9,9 @@
 1. Клонуйте репозиторій: **`git clone https://github.com/AnDr-WaY/MANshkola.git`**
 2. Перейдіть у директорію проєкту: **`cd MANshkola`**
 3. Встановіть залежності: **`pip install -r requirements.txt`**
-2. Перейдіть у директорію сайту: **`cd MAN`**
-4. Запустіть сайт: **`python manage.py runserver`**
+4. Перейдіть у директорію сайту: **`cd MAN`**
+5. Запустіть сайт: **`python manage.py runserver`**
+6. Відкрийте сайт в браузері за адресою: **`http://127.0.0.1:8000/`**
 
 
 
